0f841b743612a265f9fc101ecf36b06b41b21632
Arun <arun.k@g2tsolutions.com>
2015-11-20 23:41:35 +0530

    Home page new design

--- sites/default/themes/realia/templates/node--apartment.tpl.php
+++ sites/default/themes/realia/templates/node--apartment.tpl.php
@@ -1,76 +1,131 @@
 <?php if ($page): ?>
 
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div id="community-override">
 
-    <?php hide($content['field_type']); ?>
-    <?php hide($content['comments']); ?>
-    <?php hide($content['links']); ?>
-    <?php hide($content['field_tags']); ?>
-    <?php hide($content['field_general_amenities']); ?>
-
-    <div class="pull-left overview">
-      <div class="row">
-        <div class="span3">
-          <h2><?php print t('Overview'); ?></h2>
-          <div class="table location">
-            <?php print render($content['field_price']); ?>
-            <?php print render($content['field_contract_type']); ?>
-            <?php print render($content['field_type']); ?>
-            <?php // print render($content['field_location']); ?>
-            <?php print render($content['field_bathrooms']); ?>
-            <?php print render($content['field_bedrooms']); ?>
-            <?php print render($content['field_area']); ?>
-            <?php print render($content['field_community']); ?>
-            <?php print render($content['field_mls_']); ?>
-            <?php print render($content['field_floor_plans']); ?>
-            <?php $location = $content['locations']['#locations']; ?>
-            <?php $loc = $location[0]; ?>
-            <?php if ($loc['street']): ?>
-              <div class="field-item">
-                <label>Street :</label><span><?php echo $loc['street']; ?></span>
-              </div>
-            <?php endif; ?>
-            <?php if ($loc['city']): ?>
-              <div class="field-item">
-                <label>City :</label><span><?php echo $loc['city']; ?></span>
-              </div>
-            <?php endif; ?>
-            <?php if ($loc['province_name']): ?>
-              <div class="field-item">
-                <label>State :</label><span><?php echo $loc['province_name']; ?></span>
-              </div>
-            <?php endif; ?>
-            <?php if ($loc['postal_code']): ?>
-              <div class="field-item">
-                <label>Zip :</label><span><?php echo $loc['postal_code']; ?></span>
-              </div>
-            <?php endif; ?>
-            <?php print render($content['field_available_floor_plans']); ?>
-            <div class="text-center">
-              <a class="btn btn-success get-direction" target="_blank" href="http://maps.google.com?q=<?php echo $loc['latitude']; ?>+<?php echo $loc['longitude']; ?>">Get Direction</a>
+      <?php hide($content['field_type']); ?>
+      <?php hide($content['comments']); ?>
+      <?php hide($content['links']); ?>
+      <?php hide($content['field_tags']); ?>
+      <?php hide($content['field_general_amenities']); ?>
+      <div class="container nmrg">
+        <div class="row nmrg">
+          <div class="span4 nmrg">
+            <div id="split-tabs">
+              <label class="tabs active" href="tab1">Overview</label>
+              <label class="tabs" href="tab2">General Amenities</label>
+              <!--              <label class="tabs" href="tab3">Neighbourhood Info</label>-->
+              <label class="tabs" href="tab4">Assigned Agents</label>
+              <label class="tabs" href="tab5">Request Information</label>
             </div>
-          </div><!-- /.table -->
-        </div><!-- /.span2 -->
-      </div><!-- /.row -->
-    </div><!-- /.overview -->
-    <div class="text-center"><?php print render($content['field_image']); ?></div>
-    <?php print render($content['body']); ?>
-
-    <h2><?php print t('General amenities'); ?></h2>
-    <?php print render($content['field_general_amenities']); ?>
-    <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
-
-      <footer>
-        <?php // print render($content['field_tags']);  ?>
-        <?php // print render($content['links']);  ?>
-      </footer>
-
-    <?php endif; ?>
-
-    <?php print render($content['comments']); ?>
-
+          </div>
+          <div class="span8 nmrg">
+            <div class="split-tabs" id="tab1">
+              <h2><?php print t('Overview'); ?></h2>
+              <div class="location">
+                <?php print render($content['field_price']); ?>
+                <?php print render($content['field_contract_type']); ?>
+                <?php print render($content['field_type']); ?>
+                <?php // print render($content['field_location']); ?>
+                <?php print render($content['field_bathrooms']); ?>
+                <?php print render($content['field_bedrooms']); ?>
+                <?php print render($content['field_area']); ?>
+                <?php print render($content['field_community']); ?>
+                <?php print render($content['field_mls_']); ?>
+                <?php print render($content['field_floor_plans']); ?>
+                <?php $location = $content['locations']['#locations']; ?>
+                <?php $loc = $location[0]; ?>
+                <?php if ($loc['street']): ?>
+                  <div class="field">
+                    <div class="field-label">Street:</div>
+                    <div class="field-items">
+                      <div class="field-item">
+                        <?php echo $loc['street']; ?>
+                      </div>
+                    </div>
+                  </div>
+                <?php endif; ?>
+                <?php if ($loc['city']): ?>
+                  <div class="field">
+                    <div class="field-label">City:</div>
+                    <div class="field-items">
+                      <div class="field-item">
+                        <?php echo $loc['city']; ?>
+                      </div>
+                    </div>
+                  </div>
+                <?php endif; ?>
+                <?php if ($loc['province_name']): ?>
+                  <div class="field">
+                    <div class="field-label">State:</div>
+                    <div class="field-items">
+                      <div class="field-item">
+                        <?php echo $loc['province_name']; ?>
+                      </div>
+                    </div>
+                  </div>
+                <?php endif; ?>
+                <?php if ($loc['postal_code']): ?>
+                  <div class="field">
+                    <div class="field-label">Zip:</div>
+                    <div class="field-items">
+                      <div class="field-item">
+                        <?php echo $loc['postal_code']; ?>
+                      </div>
+                    </div>
+                  </div>
+                <?php endif; ?>
+                <div class="text-center">
+                  <a class="btn btn-success get-direction" target="_blank" href="http://maps.google.com?q=<?php echo $loc['latitude']; ?>+<?php echo $loc['longitude']; ?>">Get Direction</a>
+                </div>
+              </div>
+            </div>
+            <div class="split-tabs" id="tab2">
+              <h2><?php print t('General amenities'); ?></h2>
+              <?php print render($content['field_general_amenities']); ?>
+            </div>
+            <!--            <div class="split-tabs" id="tab3">
+                          <h2><?php print t('Neighbourhood Info'); ?></h2>
+            <?php
+//            $mapSec = module_invoke('views', 'block_view', '911c8ce7b08fc35dd333f0345e3b2ced');
+//            print render($mapSec['content']);
+            ?>
+                        </div>-->
+            <div class="split-tabs" id="tab4">
+              <h2><?php print t('Assigned Agents'); ?></h2>
+              <?php
+              $agent = module_invoke('views', 'block_view', 'agent-assigned_agents');
+              print render($agent['content']);
+              ?>
+            </div>
+            <div class="split-tabs" id="tab5">
+              <h2><?php print t('Request Information'); ?></h2>
+              <?php
+              $webform = module_invoke('webform', 'block_view', 'client-block-196');
+              print render($webform['content']);
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
   </article> <!-- /.node -->
 
 <?php elseif ($teaser): ?>
   <?php print render($content); ?>
 <?php endif; ?>
+
+<?php
+drupal_add_js("
+(function($){
+  $(document).ready(function(){
+    $('#split-tabs .tabs').click(function(){
+      var e = $(this);
+      $('.split-tabs').hide()
+      $('#split-tabs .tabs').removeClass('active');
+      e.addClass('active');
+      $('#' + e.attr('href')).show();
+    });
+    $('#split-tabs .tabs.active').trigger('click');
+  });
+})(jQuery);
+", "inline");
